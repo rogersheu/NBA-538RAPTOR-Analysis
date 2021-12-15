380b7f589598a964b2c9a88351640aab0181d63a
rogersheu <rogersheu92@gmail.com>
2021-12-14 23:57:17 -0800

    humble beginnings

---
+++ scraper.py
@@ -0,0 +1,21 @@
+# Have: RAPTOR of current season
+# To do:
+#   Automate grabbing the main table in https://projects.fivethirtyeight.com/nba-player-ratings/
+#   One big grab at all the players' pre-season projections.
+#   https://projects.fivethirtyeight.com/2022-nba-player-projections/{firstname}-{lastname}/
+
+
+def preseason_scraper():
+    pass
+
+
+
+def active_scraper():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
