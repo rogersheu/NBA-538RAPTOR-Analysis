23fa6d626b87a20e8dffb680c4bbe7bb789201f6
rogersheu <rogersheu92@gmail.com>
2021-12-28 23:53:04 -0800

    helper functions, probably one time use

--- src/current_RAPTOR_by_team.py
+++ src/current_RAPTOR_by_team.py
@@ -11,6 +11,22 @@ dir = ("C:/Users/Roger/Documents/GitHub/RAPTOR-Delta/data/dailyRAPTOR")
 fileName = f'{dir}/RAPTORratings_{shortDate}.csv'
 
 def current_RAPTOR_by_team():
+def combine_with_preseason():
+    dataPath = 'C:/Users/Roger/Documents/GitHub/RAPTOR-Delta/data'
+    preseason_file = 'C:/Users/Roger/Documents/GitHub/RAPTOR-Delta/data/RAPTOR_preseason_predictions_2022.csv'
+    df_preseason = pd.read_csv(preseason_file)
+
+    df_curr = pd.read_csv(fileName)
+
+    df_full = df_curr.merge(df_preseason, how = 'outer', on = 'Name', sort = True)
+    df_full.to_csv(f'{dataPath}/dailyRAPTOR/fullRAPTOR_{shortDate}.csv', index = False)
+
+
+def sort_by_last_name(): #Assumes a column contains names
+    df = pd.read_csv(fileName, engine = 'python')
+    df.sort_values(by='Name', ascending = True, inplace = True)
+    df.to_csv(fileName, index = False)
+
 def main():    
     combine_with_preseason()
     #current_RAPTOR_by_team()
