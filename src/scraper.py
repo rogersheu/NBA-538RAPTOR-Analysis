6012c466ce1bc0a723b6e11e9aec327fea8dac7e
rogersheu <rogersheu92@gmail.com>
2021-12-30 22:58:38 -0800

    delete comment

--- src/scraper.py
+++ src/scraper.py
@@ -7,14 +7,6 @@ from datetime import datetime
 from csv_functions import make_dir_if_nonexistent, reset_csv, write_to_csv
 import re
 
-
-# Have: RAPTOR of current season
-# To do:
-#   Automate grabbing the main table in https://projects.fivethirtyeight.com/nba-player-ratings/
-#   One big grab at all the players' pre-season projections.
-#   https://projects.fivethirtyeight.com/2022-nba-player-projections/{firstname}-{lastname}/
-
-
 shortDate = datetime.today().strftime('%Y-%m-%d')
 shortDate_nodash = shortDate.replace("-","")
 
